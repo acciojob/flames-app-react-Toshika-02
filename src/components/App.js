901c3b4f6d547896878860f4a64e0971554ca21d
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-04-23 20:51:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,96 @@
-import React, { useState } from 'react'
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-function App(){
-  const[inp1,setInp1] = useState('')
-  const[inp2,setInp2] = useState('')
-
-  const[result,setResult] = useState()
-
-
-  function myfun1(e){
-    setInp1(e.target.value)
-  }
-
-  function myfun2(e){
-    setInp2(e.target.value)
-  }
-
-
-  function myfunction(){
-    const trimmedName1 = inp1.replace(/ /g, '').toUpperCase();
-    const trimmedName2 = inp2.replace(/ /g, '').toUpperCase();
-
-    console.log(trimmedName1)
+class App extends Component {
     
-    const uniqueCharsName1 = trimmedName1.split()
-    
-    const uniqueCharsName2 = trimmedName2.split()
-
-    const remainingCharsName1 = uniqueCharsName1.filter(char => !uniqueCharsName2.includes(char));
-    const remainingCharsName2 = uniqueCharsName2.filter(char => !uniqueCharsName1.includes(char));
-
-    const combinedLength = remainingCharsName1.length + remainingCharsName2.length;
-    const resultIndex = combinedLength % 6;
-
-    const relationships = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
-    setResult(relationships[resultIndex]);
+  render() {
+    const [str1, setStr1] = useState("");
+    const [str2, setStr2] = useState("");
+    const [op, setOp] = useState(0);
+    const [opMap, setOpMap] = useState({
+      0: "Siblings",
+      1: "Friends",
+      2: "Love",
+      3: "Affection",
+      4: "Marriage",
+      5: "Enemy",
+    });
+  
+    const calculateRelation = () => {
+      const mapper1 = {};
+      const mapper2 = {};
+  
+      const resultMapper = {};
+  
+      for (let i = 0; i < str1.length; i++) {
+        if (mapper1[str1[i]]) mapper1[str1[i]] += 1;
+        else mapper1[str1[i]] = 1;
+      }
+  
+      for (let i = 0; i < str2.length; i++) {
+        if (mapper2[str2[i]]) mapper2[str2[i]] += 1;
+        else mapper2[str2[i]] = 1;
+      }
+  
+      for (let i in mapper1) {
+        if (Object.keys(mapper2).includes(i)) {
+          resultMapper[i] = mapper1[i] + mapper2[i] - 1;
+        }
+      }
+  
+      let sum = 0;
+  
+      for (let i in resultMapper) {
+        sum += parseInt(resultMapper[i]);
+      }
+  
+      console.log({ mapper1, mapper2, resultMapper, sum }, sum % 6);
+      setOp(sum % 6);
+    };
+  
+    const clear = () => {
+      setStr1("");
+      setStr2("");
+    };
+  
+    return (
+      <div id="main">
+        <div>
+          Str1 :
+          <input
+            data-testid="input1"
+            type="text"
+            value={str1}
+            onChange={(e) => {
+              const val1 = e.target.value;
+              if (val1) setStr1(val1.toLowerCase());
+            }}
+          />
+          <br />
+          Str2 :
+          <input
+            data-testid="input2"
+            type="text"
+            value={str2}
+            onChange={(e) => {
+              setStr2(e.target.value?.toLowerCase());
+            }}
+          />
+          <br />
+          <button
+            onClick={calculateRelation}
+            data-testid="calculate_relationship"
+          >
+            Calculate Relationship Future
+          </button>
+          <h3 data-testid="answer">{opMap[op]}</h3>
+          <button onClick={clear} data-testid="clear">
+            Clear
+          </button>
+        </div>
+      </div>
+    );
   }
-
-  function Clear(){
-    setInp1('')
-    setInp2('')
-    setResult('')
-  }
-
-  return(
-    <div>
-      <input data-testid="input1" name="name1"  onChange={myfun1} value={inp1}  type="text" placeholder='Enter first name'/>
-      <input data-testid="input2" name="name2" onChange={myfun2} value={inp2} type="text" placeholder='Enter second name'/>
-      <button data-testid="calculate_relationship" onClick={myfunction}>Calculate Realationship Future</button>
-      <button data-testid="clear" onClick={Clear}>Clear</button>
-      <h3 data-testid="answer">Marriage : {result}</h3>
-    </div>
-  )
 }
 
 export default App;
