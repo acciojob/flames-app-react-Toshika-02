e427003cd88218e881faa4cd0df63921bba4c142
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-04-23 20:59:14 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,96 +1,54 @@
 import React, { Component, useState } from "react";
-import "../styles/App.css";
+import '../styles/App.css';
 
-class App extends Component {
-    
-  render() {
-    const [str1, setStr1] = useState("");
-    const [str2, setStr2] = useState("");
-    const [op, setOp] = useState(0);
-    const [opMap, setOpMap] = useState({
-      0: "Siblings",
-      1: "Friends",
-      2: "Love",
-      3: "Affection",
-      4: "Marriage",
-      5: "Enemy",
-    });
-  
-    const calculateRelation = () => {
-      const mapper1 = {};
-      const mapper2 = {};
-  
-      const resultMapper = {};
-  
-      for (let i = 0; i < str1.length; i++) {
-        if (mapper1[str1[i]]) mapper1[str1[i]] += 1;
-        else mapper1[str1[i]] = 1;
-      }
-  
-      for (let i = 0; i < str2.length; i++) {
-        if (mapper2[str2[i]]) mapper2[str2[i]] += 1;
-        else mapper2[str2[i]] = 1;
-      }
-  
-      for (let i in mapper1) {
-        if (Object.keys(mapper2).includes(i)) {
-          resultMapper[i] = mapper1[i] + mapper2[i] - 1;
+const App = () => {
+    const [name1, setName1] = useState('')
+    const [name2, setName2] = useState('')
+    const [relation, setRelation] = useState('')
+
+    const calculateRelation = (str1, str2) => {
+        for (let i = 0; i < str1.length; i++) {
+            for (let j = 0; j < str2.length; j++) {
+                if (str1[i] == str2[j]) {
+                    str1 = str1.replace(str1[i], "")
+                    str2 = str2.replace(str2[j], "")
+                }
+            }
         }
-      }
-  
-      let sum = 0;
-  
-      for (let i in resultMapper) {
-        sum += parseInt(resultMapper[i]);
-      }
-  
-      console.log({ mapper1, mapper2, resultMapper, sum }, sum % 6);
-      setOp(sum % 6);
-    };
-  
-    const clear = () => {
-      setStr1("");
-      setStr2("");
-    };
-  
+        const n = (str1.length + str2.length)%6
+        if(n==0) setRelation("Siblings")
+        if(n==1) setRelation("Friends")
+        if(n==2) setRelation("Love")
+        if(n==3) setRelation("Affection")
+        if(n==4) setRelation("Marriage")
+        if(n==5) setRelation("Enemy")
+    }
+    const checkStatus = (e) => {
+        e.preventDefault()
+        if(name1=='' || name2=='') {
+            setRelation('Please Enter valid input')
+            return
+        }
+        calculateRelation(name1, name2)
+    }
+    const clearNames = () => {
+        setName1('')
+        setName2('')
+    }
+
     return (
-      <div id="main">
-        <div>
-          Str1 :
-          <input
-            data-testid="input1"
-            type="text"
-            value={str1}
-            onChange={(e) => {
-              const val1 = e.target.value;
-              if (val1) setStr1(val1.toLowerCase());
-            }}
-          />
-          <br />
-          Str2 :
-          <input
-            data-testid="input2"
-            type="text"
-            value={str2}
-            onChange={(e) => {
-              setStr2(e.target.value?.toLowerCase());
-            }}
-          />
-          <br />
-          <button
-            onClick={calculateRelation}
-            data-testid="calculate_relationship"
-          >
-            Calculate Relationship Future
-          </button>
-          <h3 data-testid="answer">{opMap[op]}</h3>
-          <button onClick={clear} data-testid="clear">
-            Clear
-          </button>
+        <div id="main">
+            {/* Do not remove the main div */}
+            <form action="" onSubmit={e => checkStatus(e)}>
+                <input name="name1" data-testid="input1" type="text" value={name1} onChange={e => setName1(e.target.value)} placeholder="Enter first name" />
+                <input name="name2" data-testid="input2" type="text" value={name2} onChange={e => setName2(e.target.value)} placeholder="Enter last name" />
+                <button data-testid="calculate_relationship" type="submit" >Calculate Relationship Future</button>
+                <button data-testid="clear" type="button" onClick={clearNames} >Clear</button>
+            </form>
+            <h3 data-testid="answer" >{relation}</h3>
         </div>
-      </div>
-    );
-  }
+    )
 }
+
 
 export default App;
